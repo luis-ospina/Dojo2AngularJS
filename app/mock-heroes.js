9303b5995d187c999c62a06e10420c1f40a4d69c
Luis Ospina <ldavid4840@gmail.com>
2016-04-02 12:32:47 -0500

    Adding files

---
+++ app/mock-heroes.js
@@ -0,0 +1,25 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var HEROES;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("HEROES", HEROES = [
+                { "id": 11, "name": "Deadpool" },
+                { "id": 12, "name": "Goku" },
+                { "id": 13, "name": "Batman" },
+                { "id": 14, "name": "Superman" },
+                { "id": 15, "name": "Capitán America" },
+                { "id": 16, "name": "Ironman" },
+                { "id": 17, "name": "Potatoman" }
+            ]);
+        }
+    }
+});
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export var HEROES: Hero[] = [
+	{"id": 11, "name": "Deadpool"},
+	{"id": 12, "name": "Goku"},
+	{"id": 13, "name": "Batman"},
+	{"id": 14, "name": "Superman"},
+	{"id": 15, "name": "Capitán America"},
+	{"id": 16, "name": "Ironman"},
+	{"id": 17, "name": "Potatoman"}
+];
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
